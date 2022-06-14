211836c3732112ce6f20712286df09aaf43d5e30
Artem026 <kris_my@mail.ru>
2022-06-14 17:36:59 +0300

    Trash

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -31,11 +31,16 @@ const Home = () => {
 
     }
 
+    const removeTodo = (id) => {
+        setTodos([...todos].filter(t => t._id !== id))
+
+    }
+
     return (
         <div className='text-white w-4/5 mx-auto'>
             <h1 className='text-2xl font-bold text-center mb-10'>Todo sim</h1>
             {todos.map(todo => (
-                <TodoItem key={todo._id} todo={todo} changeTodo={changeTodo} />
+                <TodoItem key={todo._id} todo={todo} changeTodo={changeTodo} removeTodo={removeTodo} />
             ))}
         </div>
     )
--- src/components/screens/Home/item/Check.jsx
+++ src/components/screens/Home/item/Check.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { BsCheck } from 'react-icons/bs'
+import cn from 'classnames'
 
 
 const Check = ({ isCompleted }) => {
     return (
         <div 
-            className={`border-2 rounded-lg border-pink-400 ${
-                isCompleted ? 'bg-pink-400':''
-            } w-5 h-5 mr-3 flex items-center justify-center`}
+            className={cn('border-2 rounded-lg border-pink-400 w-6 h-6 mr-3 flex items-center justify-center', {
+                'bg-pink-400': isCompleted,
+            })}
         >
             {isCompleted &&
-            <BsCheck size={26} className='text-gray-900'/>
+            <BsCheck size={24} className='text-gray-900'/>
             }
         </div>
     )
